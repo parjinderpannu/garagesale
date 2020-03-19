20d1678954ec4a1d6d98384c451d7d54f0c5cf15
PARJINDER S PANNU <parjinderpannu@gmail.com>
2020-03-19 15:02:03 -0700

    simple REST api

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+// Entry point of program
+func main() {
+
+	h := http.HandlerFunc(Echo)
+
+	if err := http.ListenAndServe("localhost:8888", h); err != nil {
+		log.Fatal(err)
+	}
+}
+
+// Echo just tells you about the request you made
+func Echo(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "You asked to", r.Method, r.URL.Path)
+}
+
+//https://golang.org/pkg/net/http/
+//https://golang.org/pkg/net/http/#ListenAndServe
+//https://golang.org/pkg/net/http/#Handler
+//https://golang.org/pkg/net/http/#HandlerFunc
