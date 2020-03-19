49fd825c1ddb39789f68a2353bee982344f6f130
PARJINDER S PANNU <parjinderpannu@gmail.com>
2020-03-19 15:13:28 -0700

    graceful shutdown initial

--- main.go
+++ main.go
@@ -3,12 +3,15 @@ package main
 import (
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
+	"time"
 )
 
 // Entry point of program
 func main() {
 
+	// Convert the Echo function to a type that implements http.Handler
 	h := http.HandlerFunc(Echo)
 
 	log.Println("listening on localhost:8888")
@@ -19,7 +22,16 @@ func main() {
 
 // Echo just tells you about the request you made
 func Echo(w http.ResponseWriter, r *http.Request) {
+
+	id := rand.Intn(1000)
+
+	fmt.Println("starting", id)
+
+	time.Sleep(3 * time.Second)
+
 	fmt.Fprintln(w, "You asked to", r.Method, r.URL.Path)
+
+	fmt.Println("ending", id)
 }
 
 //https://golang.org/pkg/net/http/
