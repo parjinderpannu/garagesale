f20b1fe4cc1871890f0fceef4b3bb957b28247f3
PARJINDER S PANNU <parjinderpannu@gmail.com>
2020-03-20 15:22:39 -0700

    remove app start more code

--- cmd/sales-admin/main.go
+++ cmd/sales-admin/main.go
@@ -1,15 +1,11 @@
 package main
 
 import (
-	"context"
 	"encoding/json"
 	"flag"
 	"log"
 	"net/http"
 	"net/url"
-	"os"
-	"os/signal"
-	"syscall"
 	"time"
 
 	"github.com/jmoiron/sqlx"
@@ -42,61 +38,6 @@ func main() {
 		}
 		log.Println("Seed data successful")
 		return
-	}
-
-	// =========================================================================
-	// Start API Service
-
-	ps := ProductService{db: db}
-
-	api := http.Server{
-		Addr:         "localhost:8000",
-		Handler:      http.HandlerFunc(ps.List),
-		ReadTimeout:  5 * time.Second,
-		WriteTimeout: 5 * time.Second,
-	}
-
-	// Make a channel to listen for errors coming from the listener. Use a
-	// buffered channel so the goroutine can exit if we don't collect this error.
-	serverErrors := make(chan error, 1)
-
-	// Start the service listening for requests.
-	go func() {
-		log.Printf("main : API listening on %s", api.Addr)
-		serverErrors <- api.ListenAndServe()
-	}()
-
-	// Make a channel to listen for an interrupt or terminate signal from the OS.
-	// Use a buffered channel because the signal package requires it.
-	shutdown := make(chan os.Signal, 1)
-	signal.Notify(shutdown, os.Interrupt, syscall.SIGTERM)
-
-	// =========================================================================
-	// Shutdown
-
-	// Blocking main and waiting for shutdown.
-	select {
-	case err := <-serverErrors:
-		log.Fatalf("error: listening and serving: %s", err)
-
-	case <-shutdown:
-		log.Println("main : Start shutdown")
-
-		// Give outstanding requests a deadline for completion.
-		const timeout = 5 * time.Second
-		ctx, cancel := context.WithTimeout(context.Background(), timeout)
-		defer cancel()
-
-		// Asking listener to shutdown and load shed.
-		err := api.Shutdown(ctx)
-		if err != nil {
-			log.Printf("main : Graceful shutdown did not complete in %v : %v", timeout, err)
-			err = api.Close()
-		}
-
-		if err != nil {
-			log.Fatalf("main : could not stop server gracefully : %v", err)
-		}
 	}
 }
 
