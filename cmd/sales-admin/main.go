eaceec4c1b9daed9d016b617cb17b28fa46cc10d
PARJINDER S PANNU <parjinderpannu@gmail.com>
2020-03-21 16:27:40 -0700

    modified:   cmd/sales-admin/main.go Contains only start api with interrupt handling

--- cmd/sales-admin/main.go
+++ cmd/sales-admin/main.go
@@ -3,10 +3,7 @@ package main
 import (
 	"flag"
 	"log"
-	"net/url"
-	"time"
 
-	"github.com/jmoiron/sqlx"
 	"github.com/parjinderpannu/garagesale/internal/platform/database"
 	"github.com/parjinderpannu/garagesale/internal/schema"
 )
@@ -37,30 +34,4 @@ func main() {
 		log.Println("Seed data successful")
 		return
 	}
-}
-
-func openDB() (*sqlx.DB, error) {
-	q := url.Values{}
-	q.Set("sslmode", "disable")
-	q.Set("timezone", "utc")
-
-	u := url.URL{
-		Scheme:   "postgres",
-		User:     url.UserPassword("postgres", "postgres"),
-		Host:     "localhost",
-		Path:     "postgres",
-		RawQuery: q.Encode(),
-	}
-
-	return sqlx.Open("postgres", u.String())
-}
-
-//Product is an item we sell.
-type Product struct {
-	ID          string    `db:"product_id" json:"id"`
-	Name        string    `db:"name" json:"name"`
-	Cost        int       `db:"cost" json:"cost"`
-	Quantity    int       `db:"quantity" json:"quantity"`
-	DateCreated time.Time `db:"date_created" json:"date_created"`
-	DateUpdated time.Time `db:"date_updated" json:"date_updated"`
 }
