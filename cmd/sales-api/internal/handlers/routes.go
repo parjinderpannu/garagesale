a244b6116de6c2011a4433be72f8256c965f522f
PARJINDER S PANNU <parjinderpannu@gmail.com>
2020-03-30 18:16:02 -0700

    get sales and post sales working

--- cmd/sales-api/internal/handlers/routes.go
+++ cmd/sales-api/internal/handlers/routes.go
@@ -19,5 +19,8 @@ func API(logger *log.Logger, db *sqlx.DB) http.Handler {
 	app.Handle(http.MethodPost, "/v1/products", p.Create)
 	app.Handle(http.MethodGet, "/v1/products/{id}", p.Retrieve)
 
+	app.Handle(http.MethodPost, "/v1/products/{id}/sales", p.AddSale)
+	app.Handle(http.MethodGet, "/v1/products/{id}/sales", p.ListSales)
+
 	return app
 }
