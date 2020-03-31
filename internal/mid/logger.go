9379ec6c23ad1bff18a7520dee56445673eff67f
PARJINDER S PANNU <parjinderpannu@gmail.com>
2020-03-31 09:24:53 -0700

    17.5 logging mid : time.Since(start)

--- internal/mid/logger.go
+++ internal/mid/logger.go
@@ -3,6 +3,7 @@ package mid
 import (
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/parjinderpannu/garagesale/internal/platform/web"
 )
@@ -15,12 +16,13 @@ func Logger(log *log.Logger) web.Middleware {
 
 		h := func(w http.ResponseWriter, r *http.Request) error {
 
+			start := time.Now()
 			// Run the handler chain and catch any propagated error.
 			err := before(w, r)
 
 			log.Printf(
-				"%s %s",
-				r.Method, r.URL.Path,
+				"%s %s (%v)",
+				r.Method, r.URL.Path, time.Since(start),
 			)
 
 			// Return the error to be handled further up the chain.
